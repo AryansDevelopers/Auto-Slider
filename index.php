786b21f0754c8c47a7afeb399dd582a4a263759c
Aryans <80429782+AryansDevelopers@users.noreply.github.com>
2024-09-08 00:37:32 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Slider</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="inner">
+            <img src="images/1.jpg" width="300px">
+            <img src="images/2.jpg" width="300px">
+            <img src="images/3.jpg" width="300px">
+            <img src="images/4.jpg" width="300px">
+            <img src="images/5.jpg" width="300px">
+            <img src="images/6.jpg" width="300px">
+            <img src="images/7.jpg" width="300px">
+        </div>
+    </div>
+</body>
+
+</html>
